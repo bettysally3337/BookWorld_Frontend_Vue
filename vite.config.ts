cf810c93766041b81d68420c37acb46300dfdfa2
Lexi Cheng <bettysally3337@gmail.com>
2026-03-15 21:42:23 +0800

    build: set base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/BookWorld_Frontend_Vue/',
     plugins: [vue()],
     server: {
         host: true
